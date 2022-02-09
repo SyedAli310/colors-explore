0c427744a4c8708748cddf686aa8a01672236c84
SyedAli310 <alizaidi310@gmail.com>
2022-02-09 17:56:23 +0530

    implemented random border radius for all cards + added copy button functionality

--- app.js
+++ app.js
@@ -40,27 +40,71 @@ const shuffle = (array) => {
   return array;
 };
 
+const copyToClipboard = (element) => {
+  // by navigator
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(element.dataset.hex).then(
+      function () {
+        console.log(element.dataset.hex);
+        element.innerHTML =
+          '<ion-icon class="success" name="checkmark-done-outline"></ion-icon>';
+        setTimeout(() => {
+          element.innerHTML = '<ion-icon name="clipboard-outline"></ion-icon>';
+        }, 2000);
+      },
+      function (err) {
+        alert("Failed to copy: ", err);
+      }
+    );
+  } else {
+    // IE fallback
+    window.clipboardData.setData("Text", element.dataset.hex);
+  }
+};
+
+const makeRandomBorderRadii = async () => {
+  const allCards = Array.from(
+    document.querySelectorAll(".color-card .color-card-bg")
+  );
+  //console.log(allCards);
+  for (let i = 0; i < allCards.length; i++) {
+    allCards[i].style.borderRadius = `${Math.ceil(
+      Math.random() * 100 + 1
+    )}% ${Math.ceil(Math.random() * 100 + 1)}% ${Math.ceil(
+      Math.random() * 100 + 1
+    )}% ${Math.ceil(Math.random() * 100 + 1)}% / ${Math.ceil(
+      Math.random() * 100 + 1
+    )}% ${Math.ceil(Math.random() * 100 + 1)}% ${Math.ceil(
+      Math.random() * 100 + 1
+    )}% ${Math.ceil(Math.random() * 100 + 1)}%`;
+  }
+};
+
 const getColors = async (url) => {
   res.innerHTML = loader;
   const response = await fetch(url);
   const data = await response.json();
   const colors = shuffle(data);
-  setTimeout(() => {
-    res.innerHTML = "";
-    colors.forEach((color) => {
-      let colorHexValue = `#${color.thumbnailUrl.split("/")[4]}`;
-      if (colorHexValue.length < 7 && colorHexValue.length === 6) {
-        colorHexValue = colorHexValue.insert(5, "0");
-      }
-      const colorCard = document.createElement("div");
-      colorCard.classList.add("color-card");
-      colorCard.innerHTML = `
+  res.innerHTML = "";
+  colors.forEach((color) => {
+    let colorHexValue = `#${color.thumbnailUrl.split("/")[4]}`;
+    if (colorHexValue.length < 7 && colorHexValue.length === 6) {
+      colorHexValue = colorHexValue.insert(5, "0");
+    }
+    const colorCard = document.createElement("div");
+    colorCard.classList.add("color-card");
+    colorCard.innerHTML = `
             <div class='color-card-bg' style='background-color:${colorHexValue};'></div>
-            <p class='color-code'>${colorHexValue}</p>
+            <div class='color-code'>
+              <p>${colorHexValue}</p> 
+              <button class='copy-btn' onclick='copyToClipboard(this)' data-hex='${colorHexValue}'>
+                <ion-icon name="clipboard-outline"></ion-icon>
+              </button>
+            </div>
             `;
-      res.appendChild(colorCard);
-    });
-  }, 1000);
+    res.appendChild(colorCard);
+  });
+  makeRandomBorderRadii();
 };
 
 window.onload = getColors(API_URL); // get colors on page load
